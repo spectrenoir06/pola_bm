3ed98d45422270a433584004e720ef7f35855b2c
Spectre <antoine.doussaud.org>
2020-01-01 17:48:32 +0100

    photo work

--- main.lua
+++ main.lua
@@ -15,7 +15,7 @@ function qr_code(x,y,lx,txt)
 		print(tab_or_message)
 	else
 		local s = lx / #tab_or_message
-		print(#tab_or_message, #tab_or_message[1])
+		-- print(#tab_or_message, #tab_or_message[1])
 		for k,v in ipairs(tab_or_message) do
 			for l,w in ipairs(v) do
 				if w > 0 then
@@ -31,27 +31,40 @@ end
 
 function gen()
 	love.graphics.rectangle("fill", 0, 0, 512, 384*1.5)
+	print(photo:getDimensions())
 	love.graphics.draw(photo, 0, 0)
 	love.graphics.draw(dusty, 0, 384)
 	qr_code(512/2+60, 390, 340/2, "dustyfrogz.fr/photo_2019/1234.jpeg")
 end
 
 function love.load(arg)
-	render = love.graphics.newCanvas(512, 384*1.5)
+	print("start")
 
-	photo = love.graphics.newImage("printer.jpeg")
-	dusty = love.graphics.newImage("dusty.png")
+	love.filesystem.createDirectory("pola")
+	love.filesystem.createDirectory("photo")
+	love.filesystem.createDirectory("low")
 
-	love.graphics.setCanvas(render)
-		gen()
-	love.graphics.setCanvas()
-	love.filesystem.createDirectory("save")
 
-	local list = love.filesystem.getDirectoryItems("save")
+	local list = love.filesystem.getDirectoryItems("photo")
 	for k,v in ipairs(list) do
 		print(k, v)
 	end
-	render:newImageData():encode("png", "save/photo_"..#list..".png")
+	print("photo")
+	os.execute("raspistill -n -t 1 -o "..love.filesystem.getSaveDirectory().."/photo/"..#list..".jpeg")
+	os.execute("raspistill -n -t 1 -w 512 -h 384 -o "..love.filesystem.getSaveDirectory().."/low/"..#list..".jpeg")
+
+	render = love.graphics.newCanvas(512, 384*1.5)
+
+	photo = love.graphics.newImage("low/"..#list..".jpeg")
+	dusty = love.graphics.newImage("dusty.png")
+
+	print("render")
+	love.graphics.setCanvas(render)
+		gen()
+	love.graphics.setCanvas()
+	print("save")
+	render:newImageData():encode("png", "pola/"..#list..".png")
+	print("end")
 
 end
 
